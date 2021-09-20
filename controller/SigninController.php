5a3e380cddee998cbc544d5b95589f20d33723b0
Стрельцов Михаил <>
2021-09-20 17:54:52 +0300

    no message

--- controller/SigninController.php
+++ controller/SigninController.php
@@ -6,7 +6,8 @@ class SigninController extends \Edisom\Core\Controller
 	protected function __construct()
 	{	
 		parent::__construct();
-		//header('Content-Type: application/json');	
+		header('Content-Type: application/json');	
+		header('Access-Control-Allow-Origin: *');	
 	}
 	
 	public function signin()
