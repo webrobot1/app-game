e4468f502e61b61f8d3adb561577c20db6d4cbf4
Стрельцов Михаил <>
2021-09-24 20:14:08 +0300

    no message

--- model/api/MoveModel.php
+++ model/api/MoveModel.php
@@ -9,7 +9,7 @@ class MoveModel extends ApiModel
 	protected function __construct()
 	{
 		parent::__construct();
-		$this->position = $this->model::redis()->geoPos('map:'.$this->model->player['map_id'], $this->token)[0];
+		$this->position = static::redis()->geoPos('map:'.$this->player['map_id'], $this->player['token'])[0];
 		
 		if(\Edisom\App\server\model\ServerModel::PROTOCOL == 'Udp')
 			DEFINE("SPEED", 0.1);
@@ -20,12 +20,12 @@ class MoveModel extends ApiModel
 	function __destruct()
 	{
 		// установим нвоые координаты игроку
-		$this->model::redis()->geoAdd('map:'.$this->model->player['map_id'], $this->position[0], $this->position[1], $this->token);
+		static::redis()->geoAdd('map:'.$this->player['map_id'], $this->position[0], $this->position[1], $this->player['token']);
 		
 		// сообщим всем на карте что мы двинулись
-		$this->model::redis()->publish('map:'.$this->model->player['map_id'], json_encode(['players'=>[['id'=>$this->model->player['id'], 'action'=>$this->model->player['action'], 'position'=>$this->position]]],JSON_NUMERIC_CHECK));	
+		static::redis()->publish('map:'.$this->player['map_id'], json_encode(['players'=>[['id'=>$this->player['id'], 'action'=>$this->player['action'], 'position'=>$this->position]]],JSON_NUMERIC_CHECK));	
 
-		$this->model::log('Движение игрока '.$this->token);		
+		static::log('Движение игрока '.$this->player['token']);		
 		exit();
 	}
 	
@@ -34,27 +34,27 @@ class MoveModel extends ApiModel
 	{	
 		$this->position[0] = round($this->position[0], 2);
 		$this->position[1] = round($this->position[1] + SPEED, 2);
-		$this->model->player['action'] = 'move_up';
+		$this->player['action'] = 'move_up';
 	}	
 	
 	public function down()
 	{	
 		$this->position[0] = round($this->position[0], 2);
 		$this->position[1] = round($this->position[1] - SPEED, 2);
-		$this->model->player['action'] = 'move_down';
+		$this->player['action'] = 'move_down';
 	}	
 	
 	public function left()
 	{		
 		$this->position[0] = round($this->position[0] - SPEED, 2);
 		$this->position[1] = round($this->position[1], 2);
-		$this->model->player['action'] = 'move_left';
+		$this->player['action'] = 'move_left';
 	}	
 	
 	public function right()
 	{		
 		$this->position[0] = round($this->position[0] + SPEED, 2);
 		$this->position[1] = round($this->position[1], 2);
-		$this->model->player['action'] = 'move_right';
+		$this->player['action'] = 'move_right';
 	}	
 }
